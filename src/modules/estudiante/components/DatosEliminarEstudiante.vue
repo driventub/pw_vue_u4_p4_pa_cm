850c252c955612525ca58aa372311ff54d7c9df5
paulo <aldazpaulo@gmail.com>
2023-08-02 19:55:37 -0500

    coreccion eliminar

--- src/modules/estudiante/components/DatosEliminarEstudiante.vue
+++ src/modules/estudiante/components/DatosEliminarEstudiante.vue
@@ -1,22 +1,32 @@
 <template>
     <label for="">Cedula</label>
         <input v-model="cedula" type="text" />
-        <button @click="eliminarEstudiante">Eliminar</button>
-        <label for="">Alonso Perez</label>
+        <button @click="consultaEstudiantes">Verificar</button>
+        <button @click="eliminarEstudiante" v-if="verif">Eliminar</button>
+
 </template>
 
 <script>
 import { eliminarEstudianteFachada } from '../helpers/EstudianteCliente';
+import {obtenerEstudianteFachada} from '../helpers/EstudianteCliente'
     export default {
         data(){
             return{
+                verif:false,
                 cedula:null,
+                nombre:null,
+                apellido:null,
             };
         },
         methods:{
             async eliminarEstudiante(){
                 await eliminarEstudianteFachada(this.cedula)
-            }
+            },
+            async consultaEstudiantes() {
+                const data = await obtenerEstudianteFachada(this.cedula);
+            this.nombre = data.nombre;
+            this.apellido = data.apellido;
+    },
         }
 
     }
